6ed4b6840fd7048af5e75a75b855e3ea1e1a3601
Thang Nguyen <nhthang99@outlook.com>
2019-09-25 10:27:01 +0700

    Update Tutorial Functional Programming

--- oop-fp/q2-fp.py
+++ oop-fp/q2-fp.py
@@ -1,10 +1,11 @@
 from functools import reduce
 
 def flatten_a(lst):
-    r = list()
-    for ele in lst:
-        r = r + ele
-    return r
+    # r = list()
+    # for ele in lst:
+    #     r = r + ele
+    # return r
+    return [x for y in lst for x in y]
 
 def flatten_b(lst):
     if len(lst) != 0:
@@ -13,9 +14,9 @@ def flatten_b(lst):
         return []
 
 def flatten_c(lst):
-    return reduce(lambda x, y: x + y, lst)
+    return list(reduce(lambda x, y: x + y, lst))
 
 if __name__ == "__main__":
     print(flatten_a([[1,2,3],['a','b','c'],[1.1,2.1,3.1]]))
     print(flatten_b([[1,2,3],['a','b','c'],[1.1,2.1,3.1]]))
-    print(flatten_c([[1,2,3],['a','b','c'],[1.1,2.1,3.1]]))
+    print(flatten_c([[1,2,3],['a','b','c'],[1.1,2.1,3.1]])) 
